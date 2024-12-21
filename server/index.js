91d9e8fffcd249e3ad4c6d40620bb55d1062e85b
Saurabh Manikeri <139417375+manikerisaurabh@users.noreply.github.com>
2024-12-21 11:11:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express();
+const path = require("path"); // Import the 'path' module
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
@@ -9,23 +9,29 @@ const listingRoutes = require("./routes/listing.js");
 const bookingRoutes = require("./routes/booking.js");
 const userRoutes = require("./routes/user.js");
 
+const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+// Serve React static files
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-//routes
+// Routes
 app.use("/auth", authRoutes);
 app.use("/properties", listingRoutes);
 app.use("/bookings", bookingRoutes);
 app.use("/users", userRoutes);
 
+// Catch-all route for React
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
-//Mongoose setup
-const Port = 3001;
+// Mongoose setup
+const Port = process.env.PORT || 3001; // Use environment variable for the port, if available
 mongoose
   .connect(process.env.MONGO_URL, {
     dbName: "Dream_Nest",
@@ -33,6 +39,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(Port, () => console.log(`Server Port: ${Port}`));
+    app.listen(Port, () => console.log(`Server running on port: ${Port}`));
   })
   .catch((err) => console.log(`${err} did not connect`));
