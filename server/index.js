c0e17ea8edaa95b1f19f75218c665340b58ba6ca
manikerisaurabh <manikerisaurabh@gmail.com>
2024-12-21 10:57:21 +0530

    prepare for deployement

--- server/index.js
+++ server/index.js
@@ -1,32 +1,38 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require('dotenv').config()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
 
-const authRoutes = require('./routes/auth.js')
-const listingRoutes =  require('./routes/listing.js')
-const bookingRoutes = require('./routes/booking.js')
-const userRoutes = require('./routes/user.js')
+const authRoutes = require("./routes/auth.js");
+const listingRoutes = require("./routes/listing.js");
+const bookingRoutes = require("./routes/booking.js");
+const userRoutes = require("./routes/user.js");
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 //routes
-app.use('/auth',authRoutes)
-app.use('/properties',listingRoutes)
-app.use('/bookings',bookingRoutes)
-app.use('/users', userRoutes)
+app.use("/auth", authRoutes);
+app.use("/properties", listingRoutes);
+app.use("/bookings", bookingRoutes);
+app.use("/users", userRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 //Mongoose setup
 const Port = 3001;
-mongoose.connect(process.env.MONGO_URL,{
-    dbName: 'Dream_Nest',
+mongoose
+  .connect(process.env.MONGO_URL, {
+    dbName: "Dream_Nest",
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(()=>{
-    app.listen(Port,()=> console.log(`Server Port: ${Port}`));
-})
-.catch((err)=> console.log(`${err} did not connect`));
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(Port, () => console.log(`Server Port: ${Port}`));
+  })
+  .catch((err) => console.log(`${err} did not connect`));
